b38172caf736de009c2d9e33206582a42292477c
CodeKiro <kirofiniello@outlook.it>
2024-07-03 19:48:35 +0200

    scaffolding

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TO-do-list</title>
+</head>
+<body>
+    <main id="app">
+        <h1>To Do List</h1>        
+
+    </main>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.2/axios.min.js" integrity="sha512-JSCFHhKDilTRRXe9ak/FJ28dcpOJxzQaCd3Xg8MyF6XFjODhy/YMCM8HW0TFDckNHWUewW+kfvhin43hKtJxAw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+<script src="./JS/script.js"></script>
+</body>
+</html>
